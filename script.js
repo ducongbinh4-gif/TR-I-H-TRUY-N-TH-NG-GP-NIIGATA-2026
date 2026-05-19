0895aec0e9d5dd88ad0667bd315e877198139251
ducongbinh4-gif <ducongbinh4@gmail.com>
2026-05-19 20:52:47 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+const GOOGLE_SCRIPT_URL =
+  "https://script.google.com/macros/s/AKfycbxoGlhGuio9BLQm5oXMhVpPt-9nxgToA5bzDge-4XQgGSB6pXRLU3th1CGp5GF95Jgm/exec";
+
+const form = document.getElementById("registerForm");
+const message = document.getElementById("message");
+const submitBtn = document.getElementById("submitBtn");
+
+const mainContent = document.getElementById("mainContent");
+const thankYouPage = document.getElementById("thankYouPage");
+
+const participantCount = document.getElementById("participantCount");
+const representativeBox = document.getElementById("representativeBox");
+const representativeName = document.getElementById("representativeName");
+
+participantCount.addEventListener("change", () => {
+  const value = participantCount.value;
+
+  if (value !== "1" && value !== "") {
+    representativeBox.classList.remove("hidden");
+    representativeName.required = true;
+  } else {
+    representativeBox.classList.add("hidden");
+    representativeName.required = false;
+    representativeName.value = "";
+  }
+});
+
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+
+  submitBtn.disabled = true;
+  submitBtn.textContent = "Đang gửi...";
+  message.textContent = "";
+
+  const formData = new FormData(form);
+
+  formData.set("chiPhi", "5000円");
+
+  try {
+    await fetch(GOOGLE_SCRIPT_URL, {
+      method: "POST",
+      mode: "no-cors",
+      body: formData,
+    });
+
+    form.reset();
+
+    representativeBox.classList.add("hidden");
+
+    mainContent.classList.add("hidden");
+    thankYouPage.classList.remove("hidden");
+
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+
+  } catch (error) {
+    message.textContent = "Có lỗi xảy ra, vui lòng thử lại.";
+    message.className = "error";
+  }
+
+  submitBtn.disabled = false;
+  submitBtn.textContent = "Gửi đăng ký";
+});
+
+function backToForm() {
+  thankYouPage.classList.add("hidden");
+  mainContent.classList.remove("hidden");
+
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
